a65436fce2147c04e1f1f6cd77c5d9a98d92cf8c
steven-copley <freddy77>
2018-03-12 09:14:19 +1030

    back to using zoom

--- nuselect.php
+++ nuselect.php
@@ -53,9 +53,10 @@ body  {
         $(document).mouseup(function(event){
             nuUp(event);
         });
-        $('body')//.css('zoom', Number(parent.$('#sse_resize').val()) / 100)
-		.css('transform', 'scale(' + Number(parent.$('#sse_resize').val()) / 100 + ')')
-//transform: scale(.55);		
+
+        $('body').css('zoom', Number(parent.$('#sse_resize').val()) / 100)
+//		.css('transform', Number(parent.$('#sse_resize').val()) / 100)
+
 		parent.nuHasNotBeenEdited();
 		
 	}
