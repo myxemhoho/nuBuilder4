34f24a31fafef7aab43b5e6d5b6118ad0c720aa7
steven-copley <steven@nubuilder.com>
2018-09-25 13:12:37 +0930

    .

--- nupmalogin.php
+++ nupmalogin.php
@@ -1,31 +1,35 @@
 <?php
 
-        require_once('nudatabase.php');
-		
-		
-		$nuConfigDBName	= nuRunQuery('')[1];
-        $session_id     = $_REQUEST['sessid'];
-        $values         = array($session_id);
-        $sql            = "SELECT * FROM zzzzsys_session WHERE zzzzsys_session_id = ?";
-        $obj            = nuRunQuery($sql, $values);
-        $recordObj      = db_fetch_object($obj);
-        $result         = db_num_rows($obj);
-		$page			= "nupmalogout.php";	
+	require_once('nudatabase.php');
 	
-        if ( $result == 1 ) {
+	
+	$nuConfigDBName	= nuRunQuery('')[1];
+	$session_id     = $_REQUEST['sessid'];
+	$values         = array($session_id);
+	$sql            = "SELECT * FROM zzzzsys_session WHERE zzzzsys_session_id = ?";
+	$obj            = nuRunQuery($sql, $values);
+	$recordObj      = db_fetch_object($obj);
+	$result         = db_num_rows($obj);
+	$page			= "nupmalogout.php";	
 
-                $logon_info     = json_decode($recordObj->sss_access);
-                $_user          = $logon_info->session->zzzzsys_user_id;
-                $_extra_check   = $logon_info->session->global_access;
+	if ( $result == 1 ) {
 
-                if ( $_user == $nuConfigDBGlobeadminUsername AND $_extra_check == '1' ) {
-			$page = "nudb/db_structure.php?server=1&db=$nuConfigDBName";
+		$logon_info     = json_decode($recordObj->sss_access);
+		$_user          = $logon_info->session->zzzzsys_user_id;
+		$_extra_check   = $logon_info->session->global_access;
+
+		if ( $_user == $nuConfigDBGlobeadminUsername AND $_extra_check == '1' ) {
+
+			$page 		= "nudb/db_structure.php?server=1&db=$nuConfigDBName";
+			
 			setcookie("nupmalogin", "good");
+			
 		} else {
-			 setcookie("nupmalogin", "bad");
+			setcookie("nupmalogin", "bad");
 		}
-    } else {
-		 setcookie("nupmalogin", "bad");
+		
+	} else {
+		setcookie("nupmalogin", "bad");
 	}
 
 	header("Location: $page");
