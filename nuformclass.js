ad2535720131caf230fdea74362f7e53d9a25f40
steven-copley <freddy77>
2018-10-01 06:37:56 +0930

    enter a decimal number typing a comma now works.

--- nuformclass.js
+++ nuformclass.js
@@ -639,9 +639,11 @@ class nuFormObject {
 		
 		if(f[0] == 'N'){									//-- number  '456.789','N|€ 1,000.00'
 
+			var F		= nuNumberFormat(f);
+			v			= v.replace(F.decimal, '.');
+
 			if(isNaN(Number(v))){return '';}
 
-			var F		= nuNumberFormat(f);
 			var o		= v.split('.');
 			var h		= nuAddThousandSpaces(o[0], F.separator);
 			
